99a44df8161ea758f1b562f3fc1cb3277f5ee807
Nyama-f <74872487+Nyama-f@users.noreply.github.com>
2022-07-27 18:46:10 +0500

    История траназакций работает правильно

--- app/src/main/java/com/teamforce/thanksapp/model/domain/HistoryModel.kt
+++ app/src/main/java/com/teamforce/thanksapp/model/domain/HistoryModel.kt
@@ -2,7 +2,7 @@ package com.teamforce.thanksapp.model.domain
 
 import com.teamforce.thanksapp.data.response.UserTransactionsResponse
 
-class HistoryModel(
+data class HistoryModel(
     var date: Int,
     var data: List<UserTransactionsResponse>
 )
--- app/src/main/java/com/teamforce/thanksapp/presentation/fragment/HistoryFragment.kt
+++ app/src/main/java/com/teamforce/thanksapp/presentation/fragment/HistoryFragment.kt
@@ -1,13 +1,16 @@
 package com.teamforce.thanksapp.presentation.fragment
 
 import android.os.Bundle
+import android.util.SparseArray
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.chip.ChipGroup
 import com.teamforce.thanksapp.R
 import com.teamforce.thanksapp.model.domain.HistoryModel
 import com.teamforce.thanksapp.presentation.adapter.HistoryAdapter
@@ -18,6 +21,10 @@ class HistoryFragment : Fragment(), View.OnClickListener {
 
     private lateinit var viewModel: HistoryViewModel
     private lateinit var recyclerView: RecyclerView
+    private var allTransactionsList: List<HistoryModel> = emptyList()
+    private var receivedTransactionsList: List<HistoryModel> = emptyList()
+    private var sentTransactionsList: List<HistoryModel> = emptyList()
+    private val username: String = UserDataRepository.getInstance()!!.username!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,37 +44,93 @@ class HistoryFragment : Fragment(), View.OnClickListener {
 
     private fun initViews(view: View) {
         recyclerView = view.findViewById(R.id.history_rv)
+        val progressBar = view.findViewById<ProgressBar>(R.id.progressBar)
+        val chipGroup = view.findViewById<ChipGroup>(R.id.chipGroup)
 
-        UserDataRepository.getInstance()?.token?.let {
-            UserDataRepository.getInstance()?.username?.let { it1 ->
+        UserDataRepository.getInstance()?.token?.let {  token ->
+            UserDataRepository.getInstance()?.username?.let { username ->
                 viewModel.loadTransactionsList(
-                    it,
-                    it1
+                    token,
+                    username
                 )
             }
         }
-        viewModel.transactions.observe(
+        recyclerView.adapter = HistoryAdapter(username, this)
+
+        viewModel.isLoading.observe(
             viewLifecycleOwner,
-            Observer {
-                recyclerView.adapter =
-                    UserDataRepository.getInstance()?.username?.let { it1 ->
-                        val list: ArrayList<HistoryModel> = ArrayList<HistoryModel>()
-                        for (i in 0 until it.size()) {
-                            val model = it.get(it.keyAt(i))
-                            if (model != null) {
-                                list.add(model)
-                            }
-                        }
-                        HistoryAdapter(it1, list.asReversed(), this)
-                    }
+            Observer { isLoading ->
+                if (isLoading) {
+                    recyclerView.visibility = View.GONE
+                    progressBar.visibility = View.VISIBLE
+                } else {
+                    recyclerView.visibility = View.VISIBLE
+                    progressBar.visibility = View.GONE
+                }
             }
         )
+        viewModel.allTransactions.observe(
+            viewLifecycleOwner,
+            Observer {
+                allTransactionsList = sparseArrayToReversedList(it)
+                if (chipGroup.checkedChipId == R.id.chipAll) {
+                    (recyclerView.adapter as HistoryAdapter).submitList(allTransactionsList)
+                }
+            }
+        )
+        viewModel.receivedTransactions.observe(
+            viewLifecycleOwner,
+            Observer {
+                receivedTransactionsList = sparseArrayToReversedList(it)
+                if (chipGroup.checkedChipId == R.id.chipReceived) {
+                    (recyclerView.adapter as HistoryAdapter).submitList(receivedTransactionsList)
+                }
+            }
+        )
+
+        viewModel.sentTransactions.observe(
+            viewLifecycleOwner,
+            Observer {
+                sentTransactionsList = sparseArrayToReversedList(it)
+                if (chipGroup.checkedChipId == R.id.chipSent) {
+                    (recyclerView.adapter as HistoryAdapter).submitList(sentTransactionsList)
+                }
+            }
+        )
+
         viewModel.transactionsLoadingError.observe(
             viewLifecycleOwner,
             Observer {
                 Toast.makeText(requireContext(), it, Toast.LENGTH_LONG).show()
             }
         )
+
+        chipGroup.setOnCheckedChangeListener { _, checkedId ->
+            refreshRecyclerView(checkedId)
+        }
+    }
+    private fun refreshRecyclerView(checkedId: Int) {
+        val transactions: List<HistoryModel> = when (checkedId) {
+            R.id.chipAll -> allTransactionsList
+            R.id.chipReceived -> receivedTransactionsList
+            R.id.chipSent -> sentTransactionsList
+            else -> {
+                Toast.makeText(requireContext(), "Wrong chip", Toast.LENGTH_LONG).show()
+                emptyList()
+            }
+        }
+        (recyclerView.adapter as HistoryAdapter).submitList(transactions)
+    }
+
+    private fun sparseArrayToReversedList(transactions: SparseArray<HistoryModel>): List<HistoryModel> {
+        val list: ArrayList<HistoryModel> = ArrayList<HistoryModel>()
+        for (i in 0 until transactions.size()) {
+            val model = transactions.get(transactions.keyAt(i))
+            if (model != null) {
+                list.add(model)
+            }
+        }
+        return list.asReversed()
     }
 
     override fun onClick(v: View?) {}
