a377ce8dfb636e5c0b150f53037cdeb0a3e7f9b1
Nyama-f <74872487+Nyama-f@users.noreply.github.com>
2022-08-16 12:23:44 +0500

    Написал адаптер для ленты событий

---
+++ app/src/main/java/com/teamforce/thanksapp/presentation/adapter/FeedAdapter.kt
@@ -0,0 +1,63 @@
+package com.teamforce.thanksapp.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.teamforce.thanksapp.R
+import com.teamforce.thanksapp.data.response.FeedResponse
+import com.teamforce.thanksapp.databinding.ItemFeedBinding
+
+class FeedAdapter (
+    private val username: String,
+    private val dataSet: List<FeedResponse>,
+): RecyclerView.Adapter<FeedAdapter.FeedViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedViewHolder {
+        val binding = ItemFeedBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return FeedViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = dataSet.size
+
+    class FeedViewHolder(binding: ItemFeedBinding): RecyclerView.ViewHolder(binding.root){
+        val avatarUser: ImageView = binding.userAvatar
+        val dateTime: TextView = binding.dateTime
+        val senderAndReceiver: TextView = binding.senderAndReceiver
+        val likesBtn: Button = binding.likeBtn
+        val dislikesBtn: Button = binding.dislikeBtn
+        val commentBtn: Button = binding.commentBtn
+        val view: View = binding.root
+    }
+
+    override fun onBindViewHolder(holder: FeedViewHolder, position: Int) {
+        if(!dataSet[position].transaction.sender.equals(username) &&
+            !dataSet[position].transaction.recipient.equals(username)){
+            holder.senderAndReceiver.text =
+                String.format(holder.view.context.getString(
+                    R.string.someoneToSomeone),
+                    dataSet[position].transaction.recipient,
+                    "150",
+                    dataSet[position].transaction.sender)
+        }else if(!dataSet[position].transaction.sender.equals(username)){
+            holder.senderAndReceiver.text =
+                String.format(holder.view.context.getString(
+                    R.string.youFromSomeone),
+                    "150",
+                    dataSet[position].transaction.sender)
+
+        }else{
+            holder.senderAndReceiver.text =
+                String.format(holder.view.context.getString(
+                    R.string.youToSomeone),
+                    dataSet[position].transaction.recipient,
+                    "150")
+        }
+    }
+
+}
--- app/src/main/java/com/teamforce/thanksapp/presentation/fragment/FeedFragment.kt
+++ app/src/main/java/com/teamforce/thanksapp/presentation/fragment/FeedFragment.kt
@@ -18,7 +18,7 @@ class FeedFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentFeedBinding.inflate(inflater, container, false)
         return binding.root
     }
