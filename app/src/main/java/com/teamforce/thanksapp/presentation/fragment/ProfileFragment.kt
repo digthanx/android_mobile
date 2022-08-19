d69371dc2851f9be7cc1ca2fba3563f6a38a20a4
Nyama-f <74872487+Nyama-f@users.noreply.github.com>
2022-08-19 11:47:33 +0500

    Новая кнопка выхода и задизайнил кнопку профиля

--- app/src/main/java/com/teamforce/thanksapp/presentation/fragment/ProfileFragment.kt
+++ app/src/main/java/com/teamforce/thanksapp/presentation/fragment/ProfileFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.teamforce.thanksapp.R
 import com.teamforce.thanksapp.databinding.FragmentMainFlowBinding
 import com.teamforce.thanksapp.databinding.FragmentProfileBinding
@@ -20,6 +22,19 @@ class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = checkNotNull(_binding) { "Binding is null" }
 
+    private val viewModel = ProfileViewModel()
+
+    private lateinit var userTgName: TextView
+    private lateinit var userAvatar: ImageView
+    private lateinit var userFio: TextView
+
+    private lateinit var userEmail: TextView
+    private lateinit var userPhone: TextView
+
+    private lateinit var companyUser: TextView
+    private lateinit var departmentUser: TextView
+    private lateinit var hiredAt: TextView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -31,44 +46,73 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initViews(view)
-        val userTgName = binding.userNick
-        val userId = binding.userId
-        val userAvatar = binding.userAvatar
+        initViews()
+        requestData()
+        setData()
 
-        val userName = binding.usernameValueTv
-        val legalFace = binding.legalEntityValueTv
-        val departmentUser = binding.departmentValueTv
-        val hiredAt = binding.hiredValueTv
-        val mobileUser = binding.mobileValueTv
-        val emailuser = binding.emailValueTv
-        val viewModel = ProfileViewModel()
-        viewModel.initViewModel()
+    }
 
-        val token = UserDataRepository.getInstance()?.token
-        if (token != null) {
-            viewModel.loadUserProfile(token)
-        }
+     private fun requestData(){
+         val token = UserDataRepository.getInstance()?.token
+         if (token != null) {
+             viewModel.loadUserProfile(token)
+         }
+     }
+
+    private fun setData(){
         viewModel.profile.observe(viewLifecycleOwner){
-            userTgName.text = it.profile.tgName
-            userId.text = "id ${it.profile.tgId}"
+            userFio.text = String.format(requireContext().getString(R.string.userFio),
+                it.profile.surname ,it.profile.firstname, it.profile.middlename)
 
-            userName.text = it.username
-            legalFace.text = it.profile.organization
+            userTgName.text = "@" + it.profile.tgName
+
+            if(it.profile.contacts.size == 1){
+                userEmail.text = it.profile.contacts[0].contact_id
+            }else if(it.profile.contacts.size == 2){
+                userEmail.text = it.profile.contacts[0].contact_id
+                userPhone.text = it.profile.contacts[1].contact_id
+            }
+
+
+            companyUser.text = it.profile.organization
             departmentUser.text = it.profile.department
             hiredAt.text = it.profile.hiredAt
-            // Телефона пока нет
-            // Почты тоже нет
+            // Пока не знаю как изображение подгружать и как с ними работать
         }
 
-        binding.btnLogout.setOnClickListener {
-            UserDataRepository.getInstance()?.logout(requireContext())
-            activityNavController().navigateSafely(R.id.action_global_signFlowFragment)
+        binding.exitBtn.setOnClickListener {
+            showAlertDialogForExit()
         }
     }
 
-    private fun initViews(view: View) {
+    private fun showAlertDialogForExit(){
+        MaterialAlertDialogBuilder(requireContext())
+            .setMessage(resources.getString(R.string.wouldYouLikeToExit))
 
+            .setNegativeButton(resources.getString(R.string.decline)) { dialog, which ->
+                dialog.dismiss()
+            }
+            .setPositiveButton(resources.getString(R.string.accept)) { dialog, which ->
+                dialog.cancel()
+                UserDataRepository.getInstance()?.logout(requireContext())
+                activityNavController().navigateSafely(R.id.action_global_signFlowFragment)
+            }
+            .show()
+    }
+
+    private fun initViews() {
+        userTgName = binding.userTelegramId
+        userAvatar = binding.userAvatar
+        userFio = binding.userFio
+
+        userEmail = binding.emailValueTv
+        userPhone = binding.mobileValueTv
+
+        companyUser = binding.companyValueTv
+        departmentUser = binding.departmentValueTv
+        hiredAt = binding.dateStartValueTv
+
+        viewModel.initViewModel()
     }
 
     companion object {
