5cdc259af86924e589000fc1c7e52a3e3756444d
Nyama-f <74872487+Nyama-f@users.noreply.github.com>
2022-10-03 09:44:55 +0500

    Добавил viewPager2 и инициализировал первый фрагмент с деталями

--- app/src/main/java/com/teamforce/thanksapp/presentation/fragment/challenges/DetailsMainChallengeFragment.kt
+++ app/src/main/java/com/teamforce/thanksapp/presentation/fragment/challenges/DetailsMainChallengeFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.tabs.TabLayoutMediator
 import com.teamforce.thanksapp.R
 import com.teamforce.thanksapp.databinding.FragmentChallengesBinding
 import com.teamforce.thanksapp.databinding.FragmentDetailsMainChallengeBinding
@@ -20,6 +21,9 @@ class DetailsMainChallengeFragment : Fragment(R.layout.fragment_details_main_cha
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+//        TabLayoutMediator(binding.tabLayout, binding.pager){ tab, position ->
+//            tab.text = "OBJECT ${(position + 1)}"
+//        }.attach()
         binding.closeBtn.setOnClickListener {
             findNavController().navigate(
                 R.id.action_detailsMainChallengeFragment_to_challengesFragment,
---
+++ app/src/main/java/com/teamforce/thanksapp/presentation/fragment/challenges/fragmentsViewPager2/DetailsInnerChallengeFragment.kt
@@ -0,0 +1,19 @@
+package com.teamforce.thanksapp.presentation.fragment.challenges.fragmentsViewPager2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.teamforce.thanksapp.R
+import com.teamforce.thanksapp.databinding.FragmentDetailsInnerChallengeBinding
+import com.teamforce.thanksapp.databinding.FragmentDetailsMainChallengeBinding
+
+
+class DetailsInnerChallengeFragment : Fragment(R.layout.fragment_details_inner_challenge) {
+
+    private val binding: FragmentDetailsInnerChallengeBinding by viewBinding()
+
+
+}
