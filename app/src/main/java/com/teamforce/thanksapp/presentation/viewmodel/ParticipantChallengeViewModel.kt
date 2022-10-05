88d06f49266bb9dd4bebed8ed62df0a41c00a8a6
Nyama-f <74872487+Nyama-f@users.noreply.github.com>
2022-10-05 16:01:50 +0500

    Описал запрос во viewModel

--- app/src/main/java/com/teamforce/thanksapp/presentation/viewmodel/ParticipantChallengeViewModel.kt
+++ app/src/main/java/com/teamforce/thanksapp/presentation/viewmodel/ParticipantChallengeViewModel.kt
@@ -1,11 +1,55 @@
 package com.teamforce.thanksapp.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.teamforce.thanksapp.data.response.GetChallengeParticipantsResponse
+import com.teamforce.thanksapp.domain.models.profile.ProfileModel
+import com.teamforce.thanksapp.domain.repositories.ChallengeRepository
+import com.teamforce.thanksapp.utils.ResultWrapper
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class ParticipantChallengeViewModel @Inject constructor(
-
+    private val challengeRepository: ChallengeRepository
 ) : ViewModel() {
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _participants = MutableLiveData<GetChallengeParticipantsResponse>()
+    val participants: LiveData<GetChallengeParticipantsResponse> = _participants
+    private val _participantsError = MutableLiveData<String>()
+    val participantsError: LiveData<String> = _participantsError
+
+
+    fun loadParticipants(
+        challengeId: Int
+    ) {
+        _isLoading.postValue(true)
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                _isLoading.postValue(true)
+                when (val result = challengeRepository.loadParticipants(challengeId)) {
+                    is ResultWrapper.Success -> {
+                        _participants.postValue(result.value!!)
+                    }
+                    else -> {
+                        if (result is ResultWrapper.GenericError) {
+                            _participantsError.postValue(result.error + " " + result.code)
+
+                        } else if (result is ResultWrapper.NetworkError) {
+                            _participantsError.postValue("Ошибка сети")
+                        }
+                    }
+                }
+                _isLoading.postValue(false)
+            }
+        }
+    }
 }
