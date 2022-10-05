b36782f63252a58774b7ec4a6c9f601c585aef67
Nyama-f <74872487+Nyama-f@users.noreply.github.com>
2022-10-05 09:25:58 +0500

    Поправил вид тени на верстке кнопок

--- app/src/main/java/com/teamforce/thanksapp/presentation/fragment/challenges/fragmentsViewPager2/CreateReportFragment.kt
+++ app/src/main/java/com/teamforce/thanksapp/presentation/fragment/challenges/fragmentsViewPager2/CreateReportFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.teamforce.thanksapp.R
 import com.teamforce.thanksapp.databinding.FragmentCreateReportBinding
 import com.teamforce.thanksapp.databinding.FragmentProfileBinding
+import com.teamforce.thanksapp.utils.OptionsTransaction
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,5 +20,9 @@ class CreateReportFragment : Fragment(R.layout.fragment_create_report) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.closeBtn.setOnClickListener {
+            activity?.onBackPressed()
+        }
     }
 }
