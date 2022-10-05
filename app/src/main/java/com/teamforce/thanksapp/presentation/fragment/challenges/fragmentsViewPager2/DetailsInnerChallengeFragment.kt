3b1a685c4ab1a2c5d41902efe06b78682a29a0c6
Nyama-f <74872487+Nyama-f@users.noreply.github.com>
2022-10-05 13:17:55 +0500

    вернул рабочее состояние фрагменту транзакции

--- app/src/main/java/com/teamforce/thanksapp/presentation/fragment/challenges/fragmentsViewPager2/DetailsInnerChallengeFragment.kt
+++ app/src/main/java/com/teamforce/thanksapp/presentation/fragment/challenges/fragmentsViewPager2/DetailsInnerChallengeFragment.kt
@@ -7,7 +7,6 @@ import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
@@ -18,8 +17,6 @@ import com.teamforce.thanksapp.presentation.adapter.ChallengeAdapter
 import com.teamforce.thanksapp.presentation.viewmodel.DetailsInnerChallengerViewModel
 import com.teamforce.thanksapp.utils.Consts
 import com.teamforce.thanksapp.utils.OptionsTransaction
-import com.teamforce.thanksapp.utils.activityNavController
-import com.teamforce.thanksapp.utils.navigateSafely
 import dagger.hilt.android.AndroidEntryPoint
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -35,11 +32,17 @@ class DetailsInnerChallengeFragment : Fragment(R.layout.fragment_details_inner_c
 
     private var idChallenge: Int? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             idChallenge = it.getInt(ChallengeAdapter.CHALLENGER_ID)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkReportSharedPref()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -56,10 +59,6 @@ class DetailsInnerChallengeFragment : Fragment(R.layout.fragment_details_inner_c
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        checkReportSharedPref()
-    }
 
     private fun loadChallengeData(challengeId: Int?) {
         challengeId?.let {
