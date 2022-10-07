540ac1709cf5a7a56fbedb1e7a5ba56b9c7a43ea
Nyama-f <74872487+Nyama-f@users.noreply.github.com>
2022-10-07 20:01:26 +0500

    инициализация фрагмента победителей

---
+++ app/src/main/java/com/teamforce/thanksapp/presentation/fragment/challenges/fragmentsViewPager2/WinnersChallengeFragment.kt
@@ -0,0 +1,24 @@
+package com.teamforce.thanksapp.presentation.fragment.challenges.fragmentsViewPager2
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.teamforce.thanksapp.R
+import com.teamforce.thanksapp.databinding.FragmentContendersChallengeBinding
+import com.teamforce.thanksapp.presentation.viewmodel.ContendersChallengeViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class WinnersChallengeFragment : Fragment(R.layout.fragment_winners_challenge) {
+
+    private val binding: FragmentContendersChallengeBinding by viewBinding()
+
+    //private val viewModel: WinnersChallengeViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
