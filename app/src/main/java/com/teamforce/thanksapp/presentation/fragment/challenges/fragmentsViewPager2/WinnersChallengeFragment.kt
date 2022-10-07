a743d3de780dae76cc1c3dc69be29b64f1767107
Nyama-f <74872487+Nyama-f@users.noreply.github.com>
2022-10-07 21:09:23 +0500

    верстка окна победителей

--- app/src/main/java/com/teamforce/thanksapp/presentation/fragment/challenges/fragmentsViewPager2/WinnersChallengeFragment.kt
+++ app/src/main/java/com/teamforce/thanksapp/presentation/fragment/challenges/fragmentsViewPager2/WinnersChallengeFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.teamforce.thanksapp.R
 import com.teamforce.thanksapp.databinding.FragmentContendersChallengeBinding
+import com.teamforce.thanksapp.presentation.fragment.challenges.ChallengesFragment
 import com.teamforce.thanksapp.presentation.viewmodel.ContendersChallengeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,6 +17,15 @@ class WinnersChallengeFragment : Fragment(R.layout.fragment_winners_challenge) {
     private val binding: FragmentContendersChallengeBinding by viewBinding()
 
     //private val viewModel: WinnersChallengeViewModel by viewModels()
+    private var idChallenge: Int? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            idChallenge = it.getInt(ChallengesFragment.CHALLENGER_ID)
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
