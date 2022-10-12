ad41f46f1a4c57b81464c8aea010206b9e15e5ea
Nyama-f <74872487+Nyama-f@users.noreply.github.com>
2022-10-12 07:56:42 +0500

    блокировка кнпоки по статусу отчета

---
+++ app/src/main/java/com/teamforce/thanksapp/presentation/fragment/challenges/ChallengesStatus.kt
@@ -0,0 +1,10 @@
+package com.teamforce.thanksapp.presentation.fragment.challenges
+
+enum class ChallengesStatus(val value: String) {
+    YOU_ARE_CREATER("Вы создатель челленджа"),
+    YOU_CAN_SENT_REPORT("Можно отправить отчет"),
+    REPORT_SENDED("Отчет отправлен"),
+    REPORT_APPLIED("Отчет подтвержден"),
+    REPORT_REFUSED("Отчет отклонен"),
+    YOU_GOT_REWARD("Получено вознаграждение")
+}
