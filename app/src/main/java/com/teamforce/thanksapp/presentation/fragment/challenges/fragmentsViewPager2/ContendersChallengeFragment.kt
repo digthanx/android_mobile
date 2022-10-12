78a5a4f57be68ba7ebdbbece429e19fbd9279c3e
Nyama-f <74872487+Nyama-f@users.noreply.github.com>
2022-10-12 11:08:44 +0500

    вертска и логика работы окна отклонения заявки

--- app/src/main/java/com/teamforce/thanksapp/presentation/fragment/challenges/fragmentsViewPager2/ContendersChallengeFragment.kt
+++ app/src/main/java/com/teamforce/thanksapp/presentation/fragment/challenges/fragmentsViewPager2/ContendersChallengeFragment.kt
@@ -1,11 +1,18 @@
 package com.teamforce.thanksapp.presentation.fragment.challenges.fragmentsViewPager2
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
+import android.view.WindowManager
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 import com.teamforce.thanksapp.R
 import com.teamforce.thanksapp.databinding.FragmentContendersChallengeBinding
 import com.teamforce.thanksapp.presentation.adapter.ContendersAdapter
@@ -34,16 +41,16 @@ class ContendersChallengeFragment : Fragment(R.layout.fragment_contenders_challe
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val adapter = ContendersAdapter()
-        binding.contendersRv.adapter = adapter
-        binding.contendersRv.addItemDecoration(VerticalDividerItemDecorator(16, adapter.itemCount))
+        val contendersAdapter = ContendersAdapter()
+        binding.contendersRv.adapter = contendersAdapter
+        binding.contendersRv.addItemDecoration(VerticalDividerItemDecorator(16, contendersAdapter.itemCount))
         loadParticipants()
         setData()
-        adapter.applyClickListener = {reportId: Int, state: Char ->
+        contendersAdapter.applyClickListener = { reportId: Int, state: Char ->
             viewModel.checkReport(reportId, state)
         }
-        adapter.refuseClickListener = {reportId: Int, state: Char ->
-            viewModel.checkReport(reportId, state)
+        contendersAdapter.refuseClickListener = { reportId: Int, state: Char ->
+            createDialog(reportId, state)
         }
         listeningResponse()
 
@@ -81,6 +88,35 @@ class ContendersChallengeFragment : Fragment(R.layout.fragment_contenders_challe
             }else{
                 binding.noData.visibility = View.VISIBLE
             }
+        }
+    }
+
+    private fun createDialog(reportId: Int, state: Char) {
+        val builderDialog = AlertDialog.Builder(context, R.style.FullscreenDialogTheme)
+        val inflater = requireActivity().layoutInflater
+        val newListValues = inflater.inflate(R.layout.dialog_reason_for_rejection_report, null)
+        builderDialog.setView(newListValues)
+        val dialog = builderDialog.create()
+        dialog.window?.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+
+        dialog.show()
+
+        val refuseBtn = dialog.findViewById<MaterialButton>(R.id.refuse_btn)
+        val closeDialogBtn = dialog.findViewById<MaterialButton>(R.id.closeDialog_btn)
+        refuseBtn.setOnClickListener {
+            if(dialog.findViewById<TextInputEditText>(R.id.description_et)
+                    .text?.trim()?.isNotEmpty() == true){
+                viewModel.checkReport(reportId, state)
+                dialog.cancel()
+            }else{
+                Toast.makeText(requireContext(),
+                    requireContext().getString(R.string.reasonOfRejectionIsNecessary),
+                    Toast.LENGTH_LONG).show()
+            }
+
+        }
+        closeDialogBtn.setOnClickListener {
+            dialog.dismiss()
         }
     }
 
