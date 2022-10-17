ce274d6870a29e7b18f78bf71274038c7cf02e0b
bulatBurganov <bullbur96@gmail.com>
2022-10-17 20:27:32 +0300

    исправлено сохранение состояния при переходе и возвращении из экрана истории

--- app/src/main/java/com/teamforce/thanksapp/presentation/viewmodel/HistoryListViewModel.kt
+++ app/src/main/java/com/teamforce/thanksapp/presentation/viewmodel/HistoryListViewModel.kt
@@ -5,16 +5,20 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import androidx.paging.insertSeparators
 import androidx.paging.map
 import com.teamforce.thanksapp.data.request.CancelTransactionRequest
 import com.teamforce.thanksapp.data.response.HistoryItem
 import com.teamforce.thanksapp.domain.repositories.HistoryRepository
+import com.teamforce.thanksapp.presentation.fragment.historyScreen.HistoryFragment
 import com.teamforce.thanksapp.utils.Result
 import com.teamforce.thanksapp.utils.ResultWrapper
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -42,50 +46,29 @@ class HistoryListViewModel @Inject constructor(
                 ResultWrapper.NetworkError -> {}
                 is ResultWrapper.Success -> _cancellationResult.postValue(Result.Success(0))
             }
-
         }
-
     }
 
-    fun getHistory(
-        sentOnly: Int,
-        receivedOnly: Int
-    ): Flow<PagingData<HistoryItem>> {
-        return historyRepository.getHistory(
-            receivedOnly = if (receivedOnly == -1) null else receivedOnly,
-            sentOnly = if (sentOnly == -1) null else sentOnly
-        ).map {
-            it.map {
-                it
-            }
+
+    //вот так делать три поля с потоками не надо. Надо передать параметр. Исправить
+    val allHistory = historyRepository.getHistory(
+        receivedOnly = null,
+        sentOnly = null
+    ).stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = PagingData.empty()
+    ).cachedIn(viewModelScope).map {
+        it.map {
+            it
         }
-            .map {
-                it.insertSeparators<HistoryItem.UserTransactionsResponse, HistoryItem> { before, after ->
-                    if (after == null) {
-                        return@insertSeparators null
-                    }
+    }
+        .map {
+            it.insertSeparators<HistoryItem.UserTransactionsResponse, HistoryItem> { before, after ->
+                if (after == null) return@insertSeparators null
 
-                    if (before == null) {
-                        val afterTime: LocalDateTime =
-                            LocalDateTime.parse(
-                                after.updatedAt.replace(
-                                    "+03:00",
-                                    ""
-                                )
-                            )
 
-                        val label = getDateTimeLabel(afterTime)
-                        return@insertSeparators HistoryItem.DateTimeSeparator(label)
-                    }
-
-                    val beforeTime: LocalDateTime =
-                        LocalDateTime.parse(
-                            before.updatedAt.replace(
-                                "+03:00",
-                                ""
-                            )
-                        )
-
+                if (before == null) {
                     val afterTime: LocalDateTime =
                         LocalDateTime.parse(
                             after.updatedAt.replace(
@@ -94,13 +77,138 @@ class HistoryListViewModel @Inject constructor(
                             )
                         )
 
-                    if (beforeTime.dayOfMonth != afterTime.dayOfMonth) {
-                        val title = getDateTimeLabel(afterTime)
-                        HistoryItem.DateTimeSeparator(title)
-                    } else null
+                    val label = getDateTimeLabel(afterTime)
+                    return@insertSeparators HistoryItem.DateTimeSeparator(label)
                 }
+
+                val beforeTime: LocalDateTime =
+                    LocalDateTime.parse(
+                        before.updatedAt.replace(
+                            "+03:00",
+                            ""
+                        )
+                    )
+
+                val afterTime: LocalDateTime =
+                    LocalDateTime.parse(
+                        after.updatedAt.replace(
+                            "+03:00",
+                            ""
+                        )
+                    )
+
+                if (beforeTime.dayOfMonth != afterTime.dayOfMonth) {
+                    val title = getDateTimeLabel(afterTime)
+                    HistoryItem.DateTimeSeparator(title)
+                } else null
             }
+        }
+
+    val received = historyRepository.getHistory(
+        receivedOnly = 1,
+        sentOnly = null
+    ).stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = PagingData.empty()
+    ).cachedIn(viewModelScope).map {
+        it.map {
+            it
+        }
     }
+        .map {
+            it.insertSeparators<HistoryItem.UserTransactionsResponse, HistoryItem> { before, after ->
+                if (after == null) return@insertSeparators null
+
+
+                if (before == null) {
+                    val afterTime: LocalDateTime =
+                        LocalDateTime.parse(
+                            after.updatedAt.replace(
+                                "+03:00",
+                                ""
+                            )
+                        )
+
+                    val label = getDateTimeLabel(afterTime)
+                    return@insertSeparators HistoryItem.DateTimeSeparator(label)
+                }
+
+                val beforeTime: LocalDateTime =
+                    LocalDateTime.parse(
+                        before.updatedAt.replace(
+                            "+03:00",
+                            ""
+                        )
+                    )
+
+                val afterTime: LocalDateTime =
+                    LocalDateTime.parse(
+                        after.updatedAt.replace(
+                            "+03:00",
+                            ""
+                        )
+                    )
+
+                if (beforeTime.dayOfMonth != afterTime.dayOfMonth) {
+                    val title = getDateTimeLabel(afterTime)
+                    HistoryItem.DateTimeSeparator(title)
+                } else null
+            }
+        }
+
+    val sent = historyRepository.getHistory(
+        receivedOnly = null,
+        sentOnly = 1
+    ).stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = PagingData.empty()
+    ).cachedIn(viewModelScope).map {
+        it.map {
+            it
+        }
+    }
+        .map {
+            it.insertSeparators<HistoryItem.UserTransactionsResponse, HistoryItem> { before, after ->
+                if (after == null) return@insertSeparators null
+
+
+                if (before == null) {
+                    val afterTime: LocalDateTime =
+                        LocalDateTime.parse(
+                            after.updatedAt.replace(
+                                "+03:00",
+                                ""
+                            )
+                        )
+
+                    val label = getDateTimeLabel(afterTime)
+                    return@insertSeparators HistoryItem.DateTimeSeparator(label)
+                }
+
+                val beforeTime: LocalDateTime =
+                    LocalDateTime.parse(
+                        before.updatedAt.replace(
+                            "+03:00",
+                            ""
+                        )
+                    )
+
+                val afterTime: LocalDateTime =
+                    LocalDateTime.parse(
+                        after.updatedAt.replace(
+                            "+03:00",
+                            ""
+                        )
+                    )
+
+                if (beforeTime.dayOfMonth != afterTime.dayOfMonth) {
+                    val title = getDateTimeLabel(afterTime)
+                    HistoryItem.DateTimeSeparator(title)
+                } else null
+            }
+        }
 
     private fun getDateTimeLabel(dateTime: LocalDateTime): String {
         val title = dateTime.dayOfMonth.toString() + " " + getMonth(dateTime)
