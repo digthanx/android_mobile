6d95bdfa1222726a5d9c97232f9bac519788c1f7
Nyama-f <74872487+Nyama-f@users.noreply.github.com>
2022-10-31 12:54:26 +0500

    скрываю поля при пустых данных

--- app/src/main/java/com/teamforce/thanksapp/presentation/fragment/feedScreen/fragmentsViewPager2/DetailsInnerFeedFragment.kt
+++ app/src/main/java/com/teamforce/thanksapp/presentation/fragment/feedScreen/fragmentsViewPager2/DetailsInnerFeedFragment.kt
@@ -1,11 +1,20 @@
 package com.teamforce.thanksapp.presentation.fragment.feedScreen.fragmentsViewPager2
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.core.net.toUri
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.google.android.material.chip.Chip
 import com.teamforce.thanksapp.R
+import com.teamforce.thanksapp.data.entities.feed.Tag
+import com.teamforce.thanksapp.databinding.FragmentDetailsInnerFeedBinding
+import com.teamforce.thanksapp.presentation.viewmodel.feed.DetailInnerFeedViewModel
+import com.teamforce.thanksapp.utils.Consts
 import com.teamforce.thanksapp.utils.Consts.TRANSACTION_ID
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,7 +25,11 @@ import dagger.hilt.android.AndroidEntryPoint
  * create an instance of this fragment.
  */
 @AndroidEntryPoint
-class DetailsInnerFeedFragment : Fragment() {
+class DetailsInnerFeedFragment : Fragment(R.layout.fragment_details_inner_feed) {
+
+    private val binding: FragmentDetailsInnerFeedBinding by viewBinding()
+
+    private val viewModel: DetailInnerFeedViewModel by viewModels()
 
     private var transactionId: Int? = null
 
@@ -28,12 +41,54 @@ class DetailsInnerFeedFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details_inner_feed, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        transactionId?.let { viewModel.loadTransactionDetail(it) }
+        setDetail()
+    }
+
+    private fun setDetail(){
+        viewModel.dataOfTransaction.observe(viewLifecycleOwner){
+            with(binding){
+                if(it?.reason != "") reasonTransaction.text = it?.reason
+                else messageCard.visibility = View.GONE
+                if (it != null) {
+                    setTags(it.tags)
+                }
+                if(it?.photo != null){
+                    photoTv.visibility = View.VISIBLE
+                    cardViewImg.visibility = View.VISIBLE
+                    Glide.with(requireContext())
+                        .load("${Consts.BASE_URL}${it?.photo}".toUri())
+                        .centerCrop()
+                        .transition(DrawableTransitionOptions.withCrossFade())
+                        .into(binding.senderImage)
+                }else{
+                    photoTv.visibility = View.GONE
+                    cardViewImg.visibility = View.GONE
+                }
+
+            }
+        }
+    }
+
+
+    private fun setTags(tagList: List<Tag>) {
+        for (i in tagList.indices) {
+            val tagName = tagList[i].name
+            val chip: Chip = LayoutInflater.from(binding.tagsChipGroup.context)
+                .inflate(
+                    R.layout.chip_tag_example_in_history_transaction,
+                    binding.tagsChipGroup,
+                    false
+                ) as Chip
+            with(chip) {
+                setText(tagName)
+                setEnsureMinTouchTargetSize(true)
+                minimumWidth = 0
+            }
+            binding.tagsChipGroup.addView(chip)
+        }
     }
 
     companion object {
