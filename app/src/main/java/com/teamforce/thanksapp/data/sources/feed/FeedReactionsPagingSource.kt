b2af02fbd2c9a27fe0438556de642deae231cb82
Nyama-f <74872487+Nyama-f@users.noreply.github.com>
2022-11-01 14:36:27 +0500

    paging source for list of reactions

---
+++ app/src/main/java/com/teamforce/thanksapp/data/sources/feed/FeedReactionsPagingSource.kt
@@ -0,0 +1,54 @@
+package com.teamforce.thanksapp.data.sources.feed
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.teamforce.thanksapp.data.api.ThanksApi
+import com.teamforce.thanksapp.data.response.GetReactionsForTransactionsResponse
+import com.teamforce.thanksapp.utils.Consts
+import retrofit2.HttpException
+import java.io.IOException
+
+class FeedReactionsPagingSource(
+    private val api: ThanksApi,
+    private val transactionId: Int
+) : PagingSource<Int, GetReactionsForTransactionsResponse.InnerInfoLike>() {
+
+    override fun getRefreshKey(state: PagingState<Int, GetReactionsForTransactionsResponse.InnerInfoLike>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, GetReactionsForTransactionsResponse.InnerInfoLike> {
+        var pageIndex = params.key ?: 1
+
+        if (params is LoadParams.Refresh) {
+            pageIndex = 1
+        }
+
+        return try {
+            val response = api.getReactionsForTransaction(
+                transactionId = transactionId,
+                limit = Consts.PAGE_SIZE,
+                offset = pageIndex
+            )
+            val nextKey =
+                if (response.likes[0].items.isEmpty()) {
+                    null
+                } else {
+                    pageIndex + (params.loadSize / Consts.PAGE_SIZE)
+                }
+            LoadResult.Page(
+                data = response.likes[0].items,
+                prevKey = if (pageIndex == 1) null else pageIndex,
+                nextKey = nextKey
+            )
+        } catch (exception: IOException) {
+            return LoadResult.Error(exception)
+        } catch (exception: HttpException) {
+            return LoadResult.Error(exception)
+        }
+    }
+
+}
