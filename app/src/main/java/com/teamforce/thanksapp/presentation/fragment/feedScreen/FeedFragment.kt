b2994ece0bc54024f4e5d8d137d7a6eb04571400
Nyama-f <74872487+Nyama-f@users.noreply.github.com>
2022-11-02 09:53:04 +0500

    задизайнил чалики неплохо, осталось придумат как тень им добавить

--- app/src/main/java/com/teamforce/thanksapp/presentation/fragment/feedScreen/FeedFragment.kt
+++ app/src/main/java/com/teamforce/thanksapp/presentation/fragment/feedScreen/FeedFragment.kt
@@ -1,19 +1,21 @@
 package com.teamforce.thanksapp.presentation.fragment.feedScreen
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.teamforce.thanksapp.R
 import com.teamforce.thanksapp.databinding.FragmentFeedBinding
 import com.teamforce.thanksapp.presentation.adapter.feed.PagerAdapter
-import com.teamforce.thanksapp.presentation.viewmodel.feed.FeedViewModel
 import com.teamforce.thanksapp.utils.OptionsTransaction
 import com.teamforce.thanksapp.utils.ViewLifecycleDelegate
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class FeedFragment : Fragment(R.layout.fragment_feed) {
@@ -40,6 +42,9 @@ class FeedFragment : Fragment(R.layout.fragment_feed) {
             mediator?.attach()
         }
 
+//        val tabTitle = listOf("Все", "Спасибки", "Победители", "Челленджи")
+//        setCustomViewForTabItem(tabTitle)
+
         binding.profile.setOnClickListener {
             findNavController().navigate(
                 R.id.action_feedFragment_to_profileGraph, null,
@@ -47,6 +52,23 @@ class FeedFragment : Fragment(R.layout.fragment_feed) {
             )
         }
     }
+
+    // Попытка сделать кастомную вертску для tabItem результат так себе пока что
+    // tabItem не поддаются установке разной ширины для каждого tabItem
+//    private fun setCustomViewForTabItem(tabTitles: List<String>){
+//        // Iterate over all tabs and set the custom view
+//        for (i in 0 until binding.tabGroup.tabCount) {
+//            val tab: TabLayout.Tab? = binding.tabGroup.getTabAt(i)
+//            tab?.setCustomView(setFieldCustomTabItem(i, tabTitles))
+//        }
+//    }
+//
+//    private fun setFieldCustomTabItem(position: Int, tabTitles: List<String>): View{
+//        val v: View = LayoutInflater.from(context).inflate(com.teamforce.thanksapp.R.layout.tab_item_for_pager, null)
+//        val tv = v.findViewById<View>(com.teamforce.thanksapp.R.id.textViewInTabItem) as TextView
+//        tv.setText(tabTitles.get(position))
+//        return v
+//    }
 
     override fun onDestroyView() {
         mediator?.detach()
