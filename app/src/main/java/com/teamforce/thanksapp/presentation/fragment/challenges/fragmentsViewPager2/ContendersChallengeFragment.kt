736d553c356212f35a54551479f68eb958a62bf0
Nyama-f <mde.developer@gmail.com>
2022-11-14 19:14:41 +0500

    в мой результат и претендентов тоже добавил

--- app/src/main/java/com/teamforce/thanksapp/presentation/fragment/challenges/fragmentsViewPager2/ContendersChallengeFragment.kt
+++ app/src/main/java/com/teamforce/thanksapp/presentation/fragment/challenges/fragmentsViewPager2/ContendersChallengeFragment.kt
@@ -1,16 +1,25 @@
 package com.teamforce.thanksapp.presentation.fragment.challenges.fragmentsViewPager2
 
+import android.Manifest
 import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textfield.TextInputEditText
 import com.teamforce.thanksapp.R
 import com.teamforce.thanksapp.data.response.GetChallengeContendersResponse
@@ -38,6 +47,71 @@ class ContendersChallengeFragment : Fragment(R.layout.fragment_contenders_challe
 
     //private var currentPositionItem = -1
 
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (!isGranted) {
+                showDialogAboutPermissions()
+
+            }
+        }
+
+    private fun showDialogAboutPermissions() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setMessage(resources.getString(R.string.explainingAboutPermissions))
+
+            .setNegativeButton(resources.getString(R.string.close)) { dialog, _ ->
+                dialog.cancel()
+            }
+            .setPositiveButton(resources.getString(R.string.settings)) { dialog, which ->
+                dialog.cancel()
+                val reqIntent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    .apply {
+                        val uri = Uri.fromParts("package", "com.teamforce.thanksapp", null)
+                        data = uri
+                    }
+                startActivity(reqIntent)
+                // Почему то повторно не запрашивается разрешение
+                // requestPermissions()
+            }
+            .show()
+    }
+
+    private fun checkPermission(): Boolean {
+
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                return true
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE) -> {
+                showRequestPermissionRational()
+                return false
+            }
+            else -> {
+                requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                return false
+            }
+        }
+    }
+
+    private fun showRequestPermissionRational() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setMessage(resources.getString(R.string.explainingAboutPermissionsRational))
+
+            .setNegativeButton(resources.getString(R.string.close)) { dialog, _ ->
+                dialog.cancel()
+            }
+            .setPositiveButton(resources.getString(R.string.good)) { dialog, _ ->
+                requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                dialog.cancel()
+            }
+            .show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -62,14 +136,30 @@ class ContendersChallengeFragment : Fragment(R.layout.fragment_contenders_challe
                 contendersAdapter!!.itemCount
             )
         )
-        contendersAdapter?.onImageLongClicked = { clickedView, photo ->
-            showDialogAboutDownloadImage(photo, clickedView, requireContext(), lifecycleScope)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            contendersAdapter?.onImageLongClicked = { clickedView, photo ->
+                showDialogAboutDownloadImage(photo, clickedView, requireContext(), lifecycleScope)
+            }
+        }else{
+            if(checkPermission()){
+                contendersAdapter?.onImageLongClicked = { clickedView, photo ->
+                    showDialogAboutDownloadImage(photo, clickedView, requireContext(), lifecycleScope)
+                }
+            }else{
+                Toast.makeText(
+                    requireContext(),
+                    requireContext().getString(R.string.dontHaveEnoughPermissions),
+                    Toast.LENGTH_LONG).show()
+            }
         }
         loadParticipants()
         setData()
         listeners()
 
     }
+
+
 
     private fun listeners() {
 
