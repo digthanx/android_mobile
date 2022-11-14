7600baff73ef974e05be1814eac028afcd580529
Nyama-f <mde.developer@gmail.com>
2022-11-14 14:08:16 +0500

    убрал лишние дублирование тк ShapeableImageView является наслдеником ImageView

--- app/src/main/java/com/teamforce/thanksapp/utils/ImageViewExtensions.kt
+++ app/src/main/java/com/teamforce/thanksapp/utils/ImageViewExtensions.kt
@@ -41,21 +41,6 @@ fun ImageView.viewSinglePhoto(image: String, context: Context) {
     }.withHiddenStatusBar(false).show()
 }
 
-fun ShapeableImageView.viewSinglePhoto(image: String, context: Context) {
-    val images = mutableListOf<String>()
-    val fullSizeImage = image.replace("_thumb", "")
-    images.add(fullSizeImage)
-    StfalconImageViewer.Builder<String>(context, images) { imageView, image ->
-        Glide.with(this)
-            .load("${Consts.BASE_URL}${image}".toUri())
-            .fitCenter()
-            .transition(DrawableTransitionOptions.withCrossFade())
-            .error(R.drawable.ic_anon_avatar)
-            .into(imageView)
-    }.withHiddenStatusBar(false).show()
-}
-
-
 suspend fun ImageView.saveToStorage(imageUri: String, context: Context) {
     val fullSizeImage = imageUri.replace("_thumb", "")
     val imageBitmap = withContext(Dispatchers.IO) {
